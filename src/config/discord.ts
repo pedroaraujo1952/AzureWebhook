14391a04dc88ff427388016473879813c0a55af8
Ph-FMM <pedroaraujo1952@hotmail.com>
2021-05-27 17:25:35 -0400

    pullRequest create

--- src/config/discord.ts
+++ src/config/discord.ts
@@ -5,4 +5,7 @@ dotenv.config();
 export default {
   webhookId: process.env.DISCORD_WEBHOOK_ID || '',
   webhookToken: process.env.DISCORD_WEBHOOK_TOKEN || '',
+  webhookUsername: 'Azure Bot',
+  webhookAvatarURL:
+    'https://p2zk82o7hr3yb6ge7gzxx4ki-wpengine.netdna-ssl.com/wp-content/uploads/Azure-DevOps-3.png',
 };
--- src/routes/build.routes.ts
+++ src/routes/build.routes.ts
@@ -1,34 +1,11 @@
 import { Router } from 'express';
 
-import { WebhookClient } from 'discord.js';
-import discordConfig from '../config/discord';
+import { BuildController } from '../controllers/BuildController';
 
 const buildRouter = Router();
 
-buildRouter.post('/', (request, response) => {
-  const webhookClient = new WebhookClient(
-    discordConfig.webhookId,
-    discordConfig.webhookToken,
-  );
+const buildController = new BuildController();
 
-  const { message, resource } = request.body;
-
-  const { url, status } = resource;
-
-  const embed = {
-    description: message.markdown,
-    color: status === 'succeeded' ? 1879160 : 16711680,
-  };
-
-  webhookClient.send({
-    username: 'Azure Bot',
-    url: url,
-    avatarURL:
-      'https://p2zk82o7hr3yb6ge7gzxx4ki-wpengine.netdna-ssl.com/wp-content/uploads/Azure-DevOps-3.png',
-    embeds: [embed],
-  });
-
-  return response.status(204).send();
-});
+buildRouter.post('/', buildController.index);
 
 export { buildRouter };
