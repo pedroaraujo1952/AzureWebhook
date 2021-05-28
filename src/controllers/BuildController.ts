8f2a16de4cd5f3b160611a7ecdc471c0c2173eb2
Ph-FMM <pedroaraujo1952@hotmail.com>
2021-05-28 10:48:31 -0400

    aaa

--- src/controllers/BuildController.ts
+++ src/controllers/BuildController.ts
@@ -4,23 +4,19 @@ import { Request, Response } from 'express';
 import discordConfig from '../config/discord';
 
 export class BuildController {
-  private webhookClient: WebhookClient;
-
-  constructor() {
-    this.webhookClient = new WebhookClient(
+  public index(request: Request, response: Response) {
+    const webhookClient = new WebhookClient(
       discordConfig.webhookId,
       discordConfig.webhookToken,
     );
-  }
 
-  public index(request: Request, response: Response) {
     const { message, resource } = request.body;
 
     const embed = new MessageEmbed()
       .setDescription(message.markdown)
       .setColor(resource.status === 'succeeded' ? 1879160 : 16711680);
 
-    this.webhookClient.send({
+    webhookClient.send({
       username: discordConfig.webhookUsername,
       avatarURL: discordConfig.webhookAvatarURL,
       embeds: [embed],
