9ad49d6e9272f8407d4228952c95312bf5910071
Ph-FMM <pedroaraujo1952@hotmail.com>
2021-06-04 10:52:15 -0400

    fdsafs

--- src/config/discord.ts
+++ src/config/discord.ts
@@ -5,7 +5,6 @@ dotenv.config();
 export default {
   webhookId: process.env.DISCORD_WEBHOOK_ID || '',
   webhookToken: process.env.DISCORD_WEBHOOK_TOKEN || '',
-  webhookUsername: 'Azure Bot',
-  webhookAvatarURL:
-    'https://lh3.googleusercontent.com/proxy/bhj17pNLKTQ95y8kM4waSIflevf9lfQ0EaNw_w9wO__hS9x_QpruI964ZB8mlIfd53LaKIL5nY5hG3HbVUrGHQdkwb9p_QmAJ96M1IMMy4Iz0Svgjq_6IBu8tfncomd_Pmx-',
+  webhookUsername: process.env.DISCORD_WEBHOOK_USERNAME || '',
+  webhookAvatarURL: process.env.DISCORD_WEBHOOK_IMAGE_URL || '',
 };
